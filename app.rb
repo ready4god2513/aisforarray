ff9459470d17e2ea5d0dfddf61740c067d0c0ff8
Brandon Hansen <brandonh@ibethel.org>
2013-06-03 11:01:01 -0700

    Use template system

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ set :public_folder, "public"
 
 get "/" do
   cache_control :public, max_age: 60
-  send_file File.expand_path("index.html", settings.public_folder)
+  erb :index
+end
+
+get "/purpose.html" do
+  cache_control :public, max_age: 60
+  erb :purpose
 end
 
 not_found do
